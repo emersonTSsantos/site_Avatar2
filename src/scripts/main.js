57a569176034466e8cf222cb557a760cab5ad9fd
Emerson Teixeira <emerson@exemplo.com>
2024-03-13 11:41:15 -0300

    mexendo na parte responsiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,26 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
             const tabId = this.getAttribute('data-tab-button');
             const tab = document.querySelector(`[data-tab-id="${tabId}"]`);
 
-            // Pausar o vídeo se estiver reproduzindo
             if (!video.paused) {
                 video.pause();
             }
 
-            // Esconder todas as abas
             tabs.forEach(tab => {
                 tab.classList.remove('shows__list--is-active');
             });
 
-            // Remover a classe de ativo de todos os botões
             buttons.forEach(button => {
                 button.classList.remove('shows__tabs__button--is-active');
             });
 
-            // Adicionar classe de ativo ao botão clicado
             this.classList.add('shows__tabs__button--is-active');
 
-            // Exibir a aba correspondente ao botão clicado
             tab.classList.add('shows__list--is-active');
         });
     });
-});
+});   
